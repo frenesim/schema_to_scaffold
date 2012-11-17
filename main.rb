f8d2c5c8b5dc3f4ce3f22c933780b5caa5932e80
João Soares <jsoares@tsx200.(none)>
2012-11-17 00:36:27 +0000

    Refactors code to improve readability
    Changes split on /create_table/ to /create_/ allowing to use the
    'table' word to make it easier to scan for the table name.
    Changes open file method to properly close the file after reading.
    Adds a block to String#scan call using the captures passed to it to
    print the options.
    
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- main.rb
+++ main.rb
@@ -1,17 +1,14 @@
 require 'active_support/inflector'
 
-file = File.open("C:/Users/hpinto/git/gestorreservas/db/schema.rb", "r")
-schema = file.read.split(/create_table/)
-table=Array.new
-schema.each_with_index do |s,i| 
-  table<<s.scan(/"(.*)".*\|$/)
-  if i!=0 then puts "#{i}. #{table[i][0][0]}" end
+schema = File.open("./schema.rb", "r") {|f| f.read.split(/create_/) }
+puts "Loaded tables:"
+tables = schema.map.with_index do |s,i|
+  s.scan(/table "(.*)"/) {|table| puts "#{i}. #{table.first}"; table }
 end
-i = [(print 'choose model:'), gets.rstrip][1]
-t = table[i.to_i][0][0]
-puts "showing table #{t} fields"
-scaffold_model_fields = String.new
-schema[i.to_i].scan(/t\.(\w+)\s+"([a-z0-9_]+)/).each { |s| scaffold_model_fields << "#{s[1]}:#{s[0]} "}
+i = [(print 'choose model:'), gets.rstrip.to_i][1]
+t = tables[i]
+puts "Showing #{t} fields"
+scaffold_model_fields = schema[i].scan(/t\.(\w+)\s+"([a-z0-9_]+)/).inject("") { |str, s| str << "#{s[1]}:#{s[0]} "}
 t="Joy"
 if t==t.singularize || t==t.camelcase; puts"";puts"I should change the table name according with rails convention" end 
 t.came
