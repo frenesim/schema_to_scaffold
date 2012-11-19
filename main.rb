e40b127751b4ad327375527e4f099d4ae2c99c8c
frenesim <hlsp999@gmail.com>
2012-11-19 10:33:52 +0000

    The code was not working at all... removed two nonsense lines

--- main.rb
+++ main.rb
@@ -12,7 +12,5 @@ t = table[i.to_i][0][0]
 puts "showing table #{t} fields"
 scaffold_model_fields = String.new
 schema[i.to_i].scan(/t\.(\w+)\s+"([a-z0-9_]+)/).each { |s| scaffold_model_fields << "#{s[1]}:#{s[0]} "}
-t="Joy"
 if t==t.singularize || t==t.camelcase; puts"";puts"I should change the table name according with rails convention" end 
-t.came
 puts "rails generate scaffold #{t.singularize.camelize} #{scaffold_model_fields}"
