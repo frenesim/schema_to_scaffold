7db194fdc2fd912c575df036152cd7329588ac29
frenesim <hlsp999@gmail.com>
2012-11-19 18:51:25 +0000

    Inicial try to include a previous menu from which the user can pick a schema, depending from the project he is working, and generate the scaffold string afterwards

--- main.rb
+++ main.rb
@@ -1,11 +1,23 @@
 require 'active_support/inflector'
+require 'find'
 
-schema = File.open("C:/Users/hpinto/git/gestorreservas/db/schema.rb", "r") {|f| f.read.split(/create_/) }
-schema.shift
-tables = schema.map { |s| s.scan(/table "(.*)"/).first }
-tables.each_with_index{|tbl,i|  puts "#{i}. #{tbl.first}"}
-i = [(print 'Select a Table:'), gets.rstrip.to_i][1]
-t = tables[i].first
-scaffold_model_fields = schema[i].scan(/t\.(\w+)\s+"([a-z0-9_]+)/).inject("") { |str, s| str << "#{s[1]}:#{s[0]} "}
-if t==t.singularize || t==t.camelcase; puts"";puts"You should change the name of the table according with rails convention" end 
-puts "rails generate scaffold #{t.singularize.camelize} #{scaffold_model_fields}"
+paths =  Array.new
+Find.find('C:/Users/hpinto/git/') do |path|
+  path.match(/.*schema\.rb$/) ? paths<<path : nil
+end
+
+
+
+def schema2scaffold (path)
+  schema = File.open(path, "r") {|f| f.read.split(/create_/) }
+  schema.shift
+  tables = schema.map { |s| s.scan(/table "(.*)"/).first }
+  tables.each_with_index{|tbl,i|  puts "#{i}. #{tbl.first}"}
+  i = [(print 'Select a Table:'), gets.rstrip.to_i][1]
+  t = tables[i].first
+  scaffold_model_fields = schema[i].scan(/t\.(\w+)\s+"([a-z0-9_]+)/).inject("") { |str, s| str << "#{s[1]}:#{s[0]} "}
+  if t==t.singularize || t==t.camelcase; puts"";puts"You should change the name of the table according with rails convention" end 
+  puts "rails generate scaffold #{t.singularize.camelize} #{scaffold_model_fields}"
+end
+
+schema2scaffold(paths[0])
