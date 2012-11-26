c81d3c8d37f0dde418774984139aea88c9fb9e28
João Soares <jsoaresgeral@gmail.com>
2012-11-26 22:16:06 +0000

    Remove rails table naming conventions check
    No longer check for rails table naming conventions
    
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- lib/schema_to_scaffold/schema.rb
+++ lib/schema_to_scaffold/schema.rb
@@ -3,18 +3,12 @@ module SchemaToScaffold
 
     attr_reader :data, :tables
 
-    ERROR_MSG = "Warning: You should change the table names according to rails conventions."
-
     def initialize(data)
       @data, @tables = data, Schema.parse(data)
     end
 
     def table_names
       tables.map(&:name)
-    end
-
-    def check_rails_naming_conventions
-      raise NotFollowingConventionsError, ERROR_MSG if table_names.any? {|n| n == n.singularize or n == n.camelcase }
     end
 
     def table(id)
