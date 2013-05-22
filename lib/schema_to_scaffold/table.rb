dde3ca25e3f636e40c7b58c58afbb697d1bf23cb
Humberto <hlsp999@gmail.com>
2013-05-22 18:48:12 +0100

    modelizing table name

--- lib/schema_to_scaffold/table.rb
+++ lib/schema_to_scaffold/table.rb
@@ -2,6 +2,7 @@ module SchemaToScaffold
   require 'active_support/inflector'
   ##
   # fetch table names and convert them to a scaffold syntax
+
   class Table
 
     attr_reader :attributes, :name
@@ -11,8 +12,8 @@ module SchemaToScaffold
     end
 
     def to_script(target)
-      return "rails generate #{target} #{name} #{attributes.map(&:to_script).join(' ')}" if target == "scaffold"
-      return "rails generate #{target} #{name.camelize.singularize} #{attributes.map(&:to_script).join(' ')}" if target == "factory_girl:model"
+      return "rails generate #{target} #{modelize name} #{attributes.map(&:to_script).join(' ')}" if target == "scaffold"
+      return "rails generate #{target} #{modelize name} #{attributes.map(&:to_script).join(' ')}" if target == "factory_girl:model"
     end
 
     def self.parse(table_data)
@@ -21,5 +22,11 @@ module SchemaToScaffold
       atts = table_data.lines.to_a.select {|line| line =~ /t\.\w+/ }.map {|att| Attribute.parse att }
       Table.new(name, atts)
     end
+
+    private
+    def modelize (string)
+      string.camelize.singularize
+    end
+
   end
 end
